23cee3c45e483795dc4dfc926bc6e2fd38181e87
raven <losthorizon942@gmail.com>
2024-11-04 15:04:18 +0800

    add example

--- example/listeners.go
+++ example/listeners.go
@@ -2,8 +2,11 @@ package example
 
 import (
 	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/imskyd/ethers_helper"
 	"log"
+	"strings"
 )
 
 func MemPoolListen() {
@@ -18,6 +21,28 @@ func MemPoolListen() {
 			fmt.Println(subErr)
 		case tx := <-memPoolTxCh:
 			fmt.Println(tx)
+			//tx data filter
+			if len(tx.Data()) < 8 {
+				continue
+			}
+			//method filter
+			methodB := tx.Data()[:4]
+			method := common.Bytes2Hex(methodB)
+			if method != "00cb637e" {
+				continue
+			}
+
+			//from address filter
+			from, err := types.Sender(types.NewCancunSigner(tx.ChainId()), tx)
+			if err != nil {
+				from, err := types.Sender(types.HomesteadSigner{}, tx)
+				fmt.Println(from, err, tx.Hash().String())
+			}
+			if strings.EqualFold(from.String(), "0x000000000000092a51384334D8090DD869a816CB") {
+				//method data filter
+				data, err := ethers_helper.DecodeTxData("abi", common.Bytes2Hex(tx.Data()))
+				fmt.Println(data, err)
+			}
 		}
 	}
 }
